227a978f4daf97cd5a5f7c800a9d944d5ceb29f6
RitavanderTang <rvandertang@artsenzorg.nl>
2021-04-17 12:33:38 +0200

    Add SpoonClient

---
+++ MyCookbook/Clients/ISpoonClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyCookbook.Clients
+{
+    public interface ISpoonClient
+    {
+        Task<HttpResponseMessage> GetRecipes(string cuisine);
+    }
+}
---
+++ MyCookbook/Clients/SpoonClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyCookbook.Clients
+{
+    public class SpoonClient : ISpoonClient
+    {
+        private readonly HttpClient _client;
+
+        public SpoonClient(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentException(nameof(client));
+        }
+
+        public async Task<HttpResponseMessage> GetRecipes(string cuisine)
+        {
+            var apiToken = "a888ac682b0548efb1ff187ae30d692e";
+            return await _client.GetAsync($"recipes/complexSearch?apiKey={apiToken}&cuisine={cuisine}");
+        }
+    }
+}
